ba829de61d607c90719d509d4f093336b83fe3c0
DualBroke <85817455+DualBroke@users.noreply.github.com>
2021-07-10 10:38:26 +0900

    Update barrack1.py

--- barrack1.py
+++ barrack1.py
@@ -6,26 +6,6 @@ access = "H1daculIk3hJlFiaxbPTcdkRUdLX5xq1B7SFYIDS"
 secret = "dW6fFteihFuSXMVMnIaMUE32tHRhTJ6E0sFkV1Zv"
 
 
-def get_sum_DOGE_price():
-    sum_DOGE_price = get_balance("DOGE") * get_current_price("KRW-DOGE")
-    return sum_DOGE_price
-
-
-def get_sum_BTC_price():
-    sum_BTC_price = get_balance("BTC") * get_current_price("KRW-BTC")
-    return sum_BTC_price
-
-
-def get_sum_TFUEL_price():
-    sum_TFUEL_price = get_balance("TFUEL") * get_current_price("KRW-TFUEL")
-    return sum_TFUEL_price
-
-
-def get_sum_ETH_price():
-    sum_ETH_price = get_balance("ETH") * get_current_price("KRW-ETH")
-    return sum_ETH_price
-
-
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
@@ -61,32 +41,26 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
-n = 1
-n2 = 0
+
 # 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
-        print(now)
         start_time = get_start_time("KRW-DOGE")
         end_time = start_time + datetime.timedelta(days=1)
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-DOGE", 0.9)
-            current_price = get_current_price("KRW-DOGE")
-            if target_price < current_price < (target_price + 1.1):
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-DOGE", krw*0.9995)
-                    at_that_time = get_current_price("KRW-DOGE")
-                    btc = get_balance("DOGE")
-                    if btc > 5000 / get_current_price("KRW-DOGE"):
-                        if get_current_price("KRW-DOGE") > target_price * 1.05:
-                            upbit.sell_market_order(
-                                "KRW-DOGE", btc)
 
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-DOGE", 0.3)
+            current_price = get_current_price("KRW-DOGE")
+            if target_price < current_price:
+                if current_price < (target_price + 1.1):
+                    krw = get_balance("KRW")
+                    if krw > 5000:
+                        upbit.buy_market_order("KRW-DOGE", krw*0.9995)
         else:
+            btc = get_balance("DOGE")
             if btc > 5000 / get_current_price("KRW-DOGE"):
-                upbit.sell_market_order("KRW-DOGE", btc)
+                upbit.sell_market_order("KRW-DOGE", btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
